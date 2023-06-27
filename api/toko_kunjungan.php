7fc5ac883d1a3086aa067fc6e5d87366db8a4dda
fi-yona <fionaavila15@gmail.com>
2023-06-28 00:29:25 +0700

    Add REST API for Toko Kunjungan

---
+++ api/toko_kunjungan.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'config.php';
+
+// Membuat koneksi ke database
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+
+// Endpoint untuk mendapatkan data rute berdasarkan token, username, dan tanggal penugasan
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $get_token = $_GET['get_token'];
+    $id_toko = $_GET['id_toko'];
+
+    $tokenQuery = "SELECT * FROM token WHERE get_token = '$get_token'";
+    $tokenResult = $conn->query($tokenQuery);
+
+    if ($tokenResult->num_rows > 0) {
+        $tokoKunjunganQuery = "SELECT nama_toko, alamat_toko, kontak_toko, id_rute, latitude_toko, longitude_toko FROM tb_toko WHERE id_toko = '$id_toko'";
+        $tokoKunjunganResult = $conn->query($tokoKunjunganQuery);
+
+        $rows = array();
+        while ($row = $tokoKunjunganResult->fetch_assoc()) {
+            $rows[] = $row;
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($rows);
+    } else {
+        header('HTTP/1.1 401 Unauthorized');
+        $response = array('message' => 'Invalid token');
+        echo json_encode($response);
+    }
+}
+
+$conn->close();
+
+?>
