cda74de9ddce299faab1c430df13a7af8c414d33
fi-yona <fionaavila15@gmail.com>
2023-06-28 20:52:13 +0700

    Add REST API for Detail Rute

---
+++ api/detail_rute.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'config.php';
+
+// Membuat koneksi ke database
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+
+// Endpoint untuk mendapatkan data rute berdasarkan token, username, dan tanggal penugasan
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $get_token = $_GET['get_token'];
+    $id_rute = $_GET['id_rute'];
+
+    $tokenQuery = "SELECT * FROM token WHERE get_token = '$get_token'";
+    $tokenResult = $conn->query($tokenQuery);
+
+    if ($tokenResult->num_rows > 0) {
+        $ruteQuery = "SELECT nama_rute FROM tb_rute WHERE id_rute = '$id_rute'";
+        $ruteResult = $conn->query($ruteQuery);
+
+        $rows = array();
+        while ($row = $ruteResult->fetch_assoc()) {
+            $rows[] = $row;
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($rows);
+    } else {
+        header('HTTP/1.1 401 Unauthorized');
+        $response = array('message' => 'Invalid token');
+        echo json_encode($response);
+    }
+}
+
+$conn->close();
+
+?>
