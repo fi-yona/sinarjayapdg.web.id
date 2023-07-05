06512bae82df3a31767e78045f5509c65b1da30b
fi-yona <fionaavila15@gmail.com>
2023-07-05 16:47:40 +0700

    Add Function View Data Detail Pembayaran

---
+++ function/data-detail-barang-pembayaran.php
@@ -0,0 +1,77 @@
+<?php
+//session_start();
+
+// Periksa apakah pengguna sudah login
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: ../../staff/login.html");
+    exit();
+}
+
+// Periksa role pengguna
+if ($_SESSION['role'] !== 'Manajer') {
+    header("Location: ../../staff/login.html");
+    echo "Anda tidak memiliki akses ke halaman ini!";
+    exit();
+}
+
+require_once 'dbconfig.php';
+
+$id_pembayaran = $_GET['id_pembayaran'];
+
+function createDetailBarangRiwayatPembayaranLink($id_dt_pembayaran)
+{
+    $link = '<a href="detail-barang-pembayaran.php?id_dt_pembayaran=' . $id_dt_pembayaran . '">Detail</a>';
+    return $link;
+}
+
+// Query SQL
+$sql = "SELECT
+            tb_detail_pembayaran.id_dt_pembayaran,
+            tb_merek.nama_merek,
+            tb_barang.nama_barang,
+            tb_detail_pembayaran.banyak_barang,
+            tb_detail_pembayaran.harga_barang,
+            tb_detail_pembayaran.total_harga_barang
+        FROM 
+            tb_detail_pembayaran 
+        INNER JOIN
+            tb_barang ON tb_detail_pembayaran.id_barang = tb_barang.id_barang
+        INNER JOIN
+            tb_merek ON tb_barang.id_merek = tb_merek.id_merek
+        WHERE 
+            id_pembayaran = '$id_pembayaran'
+        ORDER BY 
+            tb_merek.nama_merek ASC, tb_barang.nama_barang ASC";
+
+// Eksekusi query
+$result = mysqli_query($conn, $sql);
+
+// Periksa hasil query
+if (mysqli_num_rows($result) > 0) {
+    echo "<table class='table-search-result'>";
+    echo "<tr>";
+    echo "<th class='.title-atribut-data-pesanan'>Nama Merek</th>";
+    echo "<th class='.title-atribut-data-pesanan'>Nama Barang</th>";
+    echo "<th class='.title-atribut-data-pesanan'>Banyak Barang</th>";
+    echo "<th class='.title-atribut-data-pesanan'>Harga Barang</th>";
+    echo "<th class='.title-atribut-data-pesanan'>Total Pembayaran Barang</th>";
+    echo "<th class='.title-atribut-data-pesanan'>Detail</th>";
+    echo "</tr>";
+
+    // Tampilkan data dalam tabel
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['nama_merek'] . "</td>";
+        echo "<td>" . $row['nama_barang'] . "</td>";
+        echo "<td>" . number_format($row['banyak_barang'], 0, ',', '.') . "</td>";
+        echo "<td>" . number_format($row['harga_barang'], 0, ',', '.') . "</td>";
+        echo "<td>" . number_format($row['total_harga_barang'], 0, ',', '.') . "</td>";
+        echo "<td>" . createDetailBarangRiwayatPembayaranLink($row['id_dt_pembayaran']) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    // Jika query tidak mengembalikan hasil
+    echo "<p>Tidak ada data barang pembayaran.</p>";
+}
--- staff/manajer/riwayat-pembayaran/detail-pembayaran.php
+++ staff/manajer/riwayat-pembayaran/detail-pembayaran.php
@@ -14,29 +14,30 @@ if ($_SESSION['role'] !== 'Manajer') {
     exit();
 }
 
-$id_pesanan = $_GET['id_pesanan'];
+$id_pembayaran = $_GET['id_pembayaran'];
 
 require_once '../../../function/dbconfig.php';
 
 $query = "SELECT
-                tb_pesanan.tanggal_pesanan,
-                tb_pesanan.waktu_pesanan,
-                tb_karyawan.nama_lengkap,
-                tb_pesanan.username,
-                tb_toko.nama_toko,
-                tb_pesanan.total_harga_pesanan,
-                tb_pesanan.sisa_pembayaran_pesanan,
-                tb_pesanan.status_bayar_pesanan,
-                tb_pesanan.cara_penagihan,
-                tb_pesanan.jatuh_tempo
+            tb_pembayaran.tanggal_pembayaran,
+            tb_pembayaran.waktu_pembayaran,
+            tb_pembayaran.id_pesanan, 
+            tb_pesanan.tanggal_pesanan,
+            tb_toko.nama_toko,
+            tb_pembayaran.jumlah_pembayaran,
+            tb_pembayaran.metode_pembayaran,
+            tb_karyawan.nama_lengkap,
+            tb_pembayaran.username
             FROM 
-                tb_pesanan
+            tb_pembayaran
+            INNER JOIN 
+            tb_pesanan ON tb_pembayaran.id_pesanan = tb_pesanan.id_pesanan
             INNER JOIN
-                tb_toko ON tb_pesanan.id_toko = tb_toko.id_toko
-            INNER JOIN
-                tb_karyawan ON tb_pesanan.username = tb_karyawan.username
+            tb_toko ON tb_pesanan.id_toko = tb_toko.id_toko
+            INNER JOIN 
+            tb_karyawan ON tb_pembayaran.username = tb_karyawan.username
             WHERE 
-                id_pesanan = '$id_pesanan'";
+                tb_pembayaran.id_pembayaran = '$id_pembayaran'";
 
 // Eksekusi query
 $result = $conn->query($query);
@@ -60,7 +61,7 @@ $row = $result->fetch_assoc();
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Detail Pesanan</title>
+        <title>Detail Pembayaran</title>
         <link rel="stylesheet" href="../../../assets/style/style-body.css?v1.2">
         <link rel="stylesheet" href="../../../assets/style/style-button.css">
         <link rel="stylesheet" href="../../../assets/style/style-img.css">
@@ -81,8 +82,8 @@ $row = $result->fetch_assoc();
                     <li><a href="../kunjungan/kunjungan.php">Kunjungan</a></li>
                     <li><a href="../toko/toko.php">Toko</a></li>
                     <li><a href="../barang/barang.php">Barang</a></li>
-                    <li><a href="./riwayat-pesanan.php">Riwayat Pesanan</a></li>
-                    <li><a href="../riwayat-pembayaran/riwayat-pembayaran.php">Riwayat Pembayaran</a></li>
+                    <li><a href="../riwayat-pesanan/riwayat-pesanan.php">Riwayat Pesanan</a></li>
+                    <li><a href="./riwayat-pembayaran.php">Riwayat Pembayaran</a></li>
                     <li><a href="../karyawan/karyawan.php">Karyawan</a></li>
                     <li><a href="#" onclick="logout()"><button type="button" class="btn-log-out">Log Out</button></a></li>
                 </ul>
@@ -90,19 +91,34 @@ $row = $result->fetch_assoc();
         </header>
         <main>
             <div class = "column-button-sub-menu">
-                <a href="javascript:history.back()"><button type="button" class="button-sub-menu-back">Kembali</button></a>
+                <a href="./riwayat-pembayaran.php"><button type="button" class="button-sub-menu-back">Kembali</button></a>
             </div>
             <div class = "title-page">
-                Detail Pesanan
+                Detail Pembayaran
             </div>
             <div class = "detail-data">
                 <div class="box-white">
                     <div class = "layout-table-absensi">
                         <table class = "table-data-absensi">
                             <tr>
+                                <th>Id Pembayaran</th>
+                                <td> : </td>
+                                <td><?php echo $id_pembayaran; ?></td>
+                            </tr>
+                            <tr>
+                                <th>Tanggal Pembayaran</th>
+                                <td> : </td>
+                                <td><?php echo $row['tanggal_pembayaran']; ?></td>
+                            </tr>
+                            <tr>
+                                <th>Waktu Pembayaran</th>
+                                <td> : </td>
+                                <td><?php echo $row['waktu_pembayaran']; ?></td>
+                            </tr>
+                            <tr>
                                 <th>Id Pesanan</th>
                                 <td> : </td>
-                                <td><?php echo $id_pesanan; ?></td>
+                                <td><?php echo $row['id_pesanan']; ?></td>
                             </tr>
                             <tr>
                                 <th>Tanggal Pesanan</th>
@@ -110,60 +126,35 @@ $row = $result->fetch_assoc();
                                 <td><?php echo $row['tanggal_pesanan']; ?></td>
                             </tr>
                             <tr>
-                                <th>Waktu Pesanan</th>
-                                <td> : </td>
-                                <td><?php echo $row['waktu_pesanan']; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Nama Sales</th>
-                                <td> : </td>
-                                <td><?php echo $row['nama_lengkap']; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Username Sales</th>
-                                <td> : </td>
-                                <td><?php echo $row['username']; ?></td>
-                            </tr>
-                            <tr>
                                 <th>Nama Toko</th>
                                 <td> : </td>
                                 <td><?php echo $row['nama_toko']; ?></td>
                             </tr>
                             <tr>
-                                <th>Total Harga Pesanan</th>
+                                <th>Jumlah Pembayaran</th>
                                 <td> : </td>
-                                <td><?php echo number_format($row['total_harga_pesanan'], 0, ',', '.'); ?></td>
+                                <td><?php echo number_format($row['jumlah_pembayaran'], 0, ',', '.'); ?></td>
                             </tr>
                             <tr>
-                                <th>Sisa Pembayaran</th>
+                                <th>Metode Pembayaran</th>
                                 <td> : </td>
-                                <td><?php echo number_format($row['sisa_pembayaran_pesanan'], 0, ',', '.'); ?></td>
+                                <td><?php echo $row['metode_pembayaran']; ?></td>
                             </tr>
                             <tr>
-                                <th>Status Bayar</th>
+                                <th>Diterima Oleh</th>
                                 <td> : </td>
-                                <td><?php echo $row['status_bayar_pesanan']; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Cara Penagihan</th>
-                                <td> : </td>
-                                <td><?php echo $row['cara_penagihan']; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Jatuh Tempo</th>
-                                <td> : </td>
-                                <td><?php echo $row['jatuh_tempo']; ?></td>
+                                <td><?php echo $row['nama_lengkap']." (username: ".$row['username'].")"; ?></td>
                             </tr>
                         </table>
                     </div>
                 </div>
             </div>
-            <!-- data detail barang pesanan -->
+            <!-- data detail barang pembayaran -->
             <div class = "search-result">
-                <?php require_once '../../../function/data-detail-barang-pesanan.php'; ?>
+                <?php require_once '../../../function/data-detail-barang-pembayaran.php'; ?>
             </div>
             <div class = "layout-button-data">
-                <a href=""><button type="button" class="button-edit-data">Edit</button></a><a href=""><button type="button" class="button-hapus-data">Hapus</button></a>
+                <a href=""><button type="button" class="button-hapus-data">Hapus</button></a>
             </div>
         </main>
         <?php include '../../../function/footer.php'; ?>
