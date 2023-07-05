5c88cae94bdec4e489d6a22596f1fe607eae4cb9
fi-yona <fionaavila15@gmail.com>
2023-07-05 13:55:03 +0700

    Add Function View Data Pesanan Jatuh Tempo

---
+++ staff/manajer/riwayat-pesanan/jatuh-tempo.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+// Periksa apakah pengguna sudah login
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: ../../../staff/login.html");
+    exit();
+}
+
+// Periksa role pengguna
+if ($_SESSION['role'] !== 'Manajer') {
+    header("Location: ../../../staff/login.html");
+    echo "Anda tidak memiliki akses ke halaman ini!";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Data Pesanan Jatuh Tempo</title>
+		<link rel="stylesheet" href="../../../assets/style/style-body.css?v1.1">
+        <link rel="stylesheet" href="../../../assets/style/style-button.css?v1.1">
+        <link rel="stylesheet" href="../../../assets/style/style-img.css">
+        <link rel="stylesheet" href="../../../assets/style/style-input.css">
+        <link rel="shortcut icon" href="../../../assets/img/logo.svg">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="../../../script/logout1.js"></script>
+	</head>
+    <body>
+        <header>
+        <header>
+            <center>
+                <h1><img src="../../../assets/img/logo-horizontal.png" class="logo-header"></h1>
+            </center>
+            <nav class="nav-home">
+                <ul class="nav-home-ul">
+                    <li><a href="../home.php">Home</a></li>
+                    <li><a href="../absensi/absensi.php">Absensi</a></li>
+                    <li><a href="../kunjungan/kunjungan.php">Kunjungan</a></li>
+                    <li><a href="../toko/toko.php">Toko</a></li>
+                    <li><a href="../barang/barang.php">Barang</a></li>
+                    <li><a href="./riwayat-pesanan.php">Riwayat Pesanan</a></li>
+                    <li><a href="../riwayat-pembayaran/riwayat-pembayaran.php">Riwayat Pembayaran</a></li>
+                    <li><a href="../karyawan/karyawan.php">Karyawan</a></li>
+                    <li><a href="#" onclick="logout()"><button type="button" class="btn-log-out">Log Out</button></a></li>
+                </ul>
+            </nav>
+        </header>
+        </header>
+        <main>
+            <div class = "column-button-sub-menu">
+                <a href="./riwayat-pesanan.php"><button type="button" class="button-sub-menu-back">Kembali</button></a>
+            </div>
+            <div class = "title-page">
+                Data Pesanan Jatuh Tempo
+            </div>
+            <div class = "search-column">
+                <form id="form-search-absensi" class="form-search" action="../function/do-search-absensi.php" method="POST"> 
+                    <table class="table-layout-search">
+                        <tr>
+                            <td class = "td-search-tanggal">
+                                <div class="box-white-black-stroke-search">
+                                    <input type="text" placeholder="Masukkan Kata Kunci" name="kata-kunci" id="kata-kunci" class="input-kata-kunci">
+                                </div>
+                            </td>
+                            <td class = "td-button-search">
+                                <input type="submit" name="search" class="button-submit-search" value="Cari Data Riwayat Pesanan">
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+            <div class = "search-result">
+                <?php include '../../../function/data-jatuh-tempo.php'; ?>
+            </div>
+        </main>
+        <?php include '../../../function/footer.php'; ?>
+    </body>
+</html>
