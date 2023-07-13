1d65166b15cadaed6d08681ce98b7da304a29801
fi-yona <fionaavila15@gmail.com>
2023-07-14 01:15:00 +0700

    Add Function Delete Data Merek

---
+++ function/delete-data-merek.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+// Periksa apakah pengguna sudah login
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: ../staff/login.html");
+    exit();
+}
+
+// Periksa role pengguna
+if ($_SESSION['role'] !== 'Manajer') {
+    header("Location: ../staff/login.html");
+    echo "Anda tidak memiliki akses ke halaman ini!";
+    exit();
+}
+
+// Periksa apakah data merek telah dikirimkan
+if (isset($_GET['id_merek'])) {
+    // Ambil data dari form
+    $id_merek = $_GET['id_merek'];
+
+    // Lakukan proses penghapusan data ke database
+    require_once 'dbconfig.php';
+
+    // Delete data penugasan
+    $query_delete = "DELETE FROM tb_merek 
+                     WHERE id_merek = '$id_merek'";
+
+    if ($conn->query($query_delete) === true) {
+        // Jika penghapusan berhasil
+        header("Location: ../staff/manajer/barang/merek.php?status=success-delete");
+        exit();
+    } else {
+        // Jika terjadi kesalahan saat penghapusan
+        echo "Terjadi kesalahan saat menghapus data merek: " . mysqli_error($conn);
+    }
+
+    $conn->close();
+}
+?>
--- staff/manajer/barang/add-manufaktur.php
+++ staff/manajer/barang/add-manufaktur.php
@@ -19,10 +19,10 @@ if ($_SESSION['role'] !== 'Manajer') {
 <html>
     <head>
         <title>Tambah Manufaktur</title>
-		<link rel="stylesheet" href="../../../assets/style/style-body.css">
+		<link rel="stylesheet" href="../../../assets/style/style-body.css?v2.2">
         <link rel="stylesheet" href="../../../assets/style/style-button.css">
         <link rel="stylesheet" href="../../../assets/style/style-img.css">
-        <link rel="stylesheet" href="../../../assets/style/style-input.css">
+        <link rel="stylesheet" href="../../../assets/style/style-input.css?v2">
         <link rel="shortcut icon" href="../../../assets/img/logo.svg">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="../../../script/logout1.js"></script>
@@ -47,18 +47,54 @@ if ($_SESSION['role'] !== 'Manajer') {
             </nav>
         </header>
         <main>
-            <div class = "column-button-sub-menu">
-                <a href="./manufaktur.php"><button type="button" class="button-sub-menu-back">Kembali</button></a>
-            </div>
             <div class = "title-page">
                 Tambah Manufaktur
             </div>
-            <div class = "column-add-data">
-                <form id="form-add-data-toko" class="table-form-add" action="../function/add-data-toko.php" method="POST">
-                    <table class="table-add-data">
-                        
-                    </table>
-                </form>
+            <div class = "detail-data">
+                <div class = "box-green-1">
+                    <form id="form-add-data-manufaktur" class="table-form-add" action="../../../function/add-data-manufaktur.php" method="POST">
+                        <table class="table-add-data">
+                            <tr>
+                                <th>Nama Manufaktur</th>
+                                <td><input type="text" placeholder="Nama Manufaktur/Badan Usaha" name="nama_manufaktur" id="nama_manufaktur" class="input-text-add"></td>
+                            </tr>
+                            <tr>
+                                <th>Kontak</th>
+                                <td><input type="text" placeholder="Nomor Telepon Kantor/CS" name="kontak_manufaktur" id="kontak_manufaktur" class="input-text-add"></td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td><input type="text" placeholder="Email Kantor/CS" name="email_manufaktur" id="email_manufaktur" class="input-text-add"></td>
+                            </tr>
+                            <tr>
+                                <th>Website</th>
+                                <td><textarea placeholder="Website Resmi Manufaktur" name="website_manufaktur" id="website_manufaktur" class="input-text-add" rows="3"></textarea></td>
+                            </tr>
+                            <tr>
+                                <th>Alamat</th>
+                                <td><textarea placeholder="Alamat Lokasi Kantor Manufaktur" name="alamat_manufaktur" id="alamat_manufaktur" class="input-text-add" rows="10"></textarea></td>
+                            </tr>
+                            <tr>
+                                <th>Kode Pos</th>
+                                <td><input type="text" placeholder="Kode Pos Kantor" name="kode_pos_manufaktur" id="kode_pos_manufaktur" class="input-text-add"></td>
+                            </tr>
+                            <tr>
+                                <th>Kota</th>
+                                <td><input type="text" placeholder="Kota Lokasi Kantor Manufaktur" name="kota_manufaktur" id="kota_manufaktur" class="input-text-add"></td>
+                            </tr>
+                            <tr>
+                                <th>Negara</th>
+                                <td><input type="text" placeholder="Negara Lokasi Kantor Manufaktur" name="negara_manufaktur" id="negara_manufaktur" class="input-text-add"></td>
+                            </tr>
+                        </table>
+                        <div class="layout-button-submit">
+                            <input type="submit" name="add-data-manufaktur" class="button-submit-add" value="Submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class = "layout-button-data">
+                <a href="javascript:history.back()"><button type="button" class="button-hapus-data">Batal</button></a>
             </div>
         </main>
         <?php include '../../../function/footer.php'; ?>
