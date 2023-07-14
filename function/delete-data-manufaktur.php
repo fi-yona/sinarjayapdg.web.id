8d99fdfb69214b1a7dd4846105be6d15e407f889
fi-yona <fionaavila15@gmail.com>
2023-07-14 12:45:00 +0700

    Add Function Delete Data Manufaktur

---
+++ function/delete-data-manufaktur.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+// Periksa apakah pengguna sudah login
+if (!isset($_SESSION['login']) || $_SESSION['login'] !== true) {
+    header("Location: ../staff/login.html");
+    exit();
+}
+
+// Periksa role pengguna
+if ($_SESSION['role'] !== 'Manajer') {
+    header("Location: ../staff/login.html");
+    echo "Anda tidak memiliki akses ke halaman ini!";
+    exit();
+}
+
+// Periksa apakah data manufaktur telah dikirimkan
+if (isset($_GET['id_manufaktur'])) {
+    // Ambil data dari form
+    $id_manufaktur = $_GET['id_manufaktur'];
+
+    // Lakukan proses penghapusan data
+    require_once 'dbconfig.php';
+
+    // Delete data
+    $query_delete = "DELETE FROM tb_manufaktur 
+                     WHERE id_manufaktur = '$id_manufaktur'";
+
+    if ($conn->query($query_delete) === true) {
+        // Jika penghapusan berhasil
+        header("Location: ../staff/manajer/barang/manufaktur.php?status=success-delete");
+        exit();
+    } else {
+        // Jika terjadi kesalahan saat penghapusan
+        echo "Terjadi kesalahan saat menghapus data manufaktur: " . mysqli_error($conn);
+    }
+
+    $conn->close();
+}
+?>
